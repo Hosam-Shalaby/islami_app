9df811a9de333cba726a7101f3f916d3d44483ed
HossamShalabyAbdelrazek <hosamshalby1@gmail.com>
2024-02-15 02:05:04 +0200

    create branch

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:islami_app/main.dart';
+import 'package:islamic/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
