f9ba914419ce06301481828ba7e938ec6ebc3f11
HossamShalabyAbdelrazek <hosamshalby1@gmail.com>
2024-02-21 02:57:55 +0200

    Create Provider for Theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:islamic/Home/Hadeth/hadeth_details_screen.dart';
+import 'package:islamic/Home/Providers/settings_provider.dart';
 import 'package:islamic/Home/home_screen.dart';
+import 'package:provider/provider.dart';
 
 import 'Home/Quran/sura_content.dart';
 import 'Splash/splash_screen.dart';
 import 'Styles/theme_data.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+      create: (context) => SettingsProvider(), child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -15,8 +18,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var settingsProvider = Provider.of<SettingsProvider>(context);
     return MaterialApp(
-      themeMode: ThemeMode.dark,
+      themeMode: settingsProvider.currentTheme,
       theme: MyThemeData.lightTheme,
       darkTheme: MyThemeData.darkTheme,
       routes: {
